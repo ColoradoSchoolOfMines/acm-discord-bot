3601b211d3826373ed736271997f41e88eede093
Keenan D. Buckley <keenandbuckley@protonmail.com>
2022-09-22 20:01:24 -0600

    add more path constants

--- tests/test_models.py
+++ tests/test_models.py
@@ -12,25 +12,23 @@ sys.path.insert(0, parent_dir)
 
 import models
 
-def test_load_dot_env(dir: str = models.ENV_DIR):
+def test_load_dot_env(dir: str = models.STATIC_DIR):
     """ Assert that loaded .env contains all keys in models.ENVs """
     models.load_dot_env(dir=dir)
-    assert os.path.isfile(f'{dir}/.env'), f'.env '
+    assert os.path.isfile(f'{dir}/{models.ENV_FILENAME}'), f'{models.ENV_FILENAME} doesn\'t exist'
     for env in models.ENVs:
         assert (models.get_env_safe(env)
                 is not None), f'Can\'t find env \"{env.value}\"'
-
 
 def test_create_dot_env():
     """ Assert that a newly created .env contains all keys in models.ENVs """
     TEST_DIR = "temp_testing"
     assert not os.path.isfile(
-        f'{TEST_DIR}/.env'), f'{TEST_DIR}/.env already exists, unsafe to test with'
+        f'{TEST_DIR}/{models.ENV_FILENAME}'), f'{TEST_DIR}/{models.ENV_FILENAME} already exists, unsafe to test with'
     test_load_dot_env(dir=TEST_DIR)
     assert os.path.isfile(
-        f'{TEST_DIR}/.env'), f'{TEST_DIR}/.env was not created'
+        f'{TEST_DIR}/{models.ENV_FILENAME}'), f'{TEST_DIR}/{models.ENV_FILENAME} was not created'
     shutil.rmtree(TEST_DIR)
-
 
 def test_create_dot_env_dir():
     """ Assert that models.create_dot_env() still works if directory already exists """
