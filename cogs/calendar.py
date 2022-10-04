c7b2e6dc9f427ac216dbc86529fb34ce9f071d8f
Jayden Pahukula <72293021+JaydenPahukula@users.noreply.github.com>
2022-10-04 17:52:37 -0600

    added database

--- cogs/calendar.py
+++ cogs/calendar.py
@@ -1,6 +1,8 @@
 import discord
 import requests
 from discord.ext import commands
+import aiosqlite
+#from ics import Calendar, Event
 
 class Calendar(commands.Cog):
     calendar = discord.SlashCommandGroup("calendar", "calendar functionality")
@@ -11,13 +13,42 @@ class Calendar(commands.Cog):
     @calendar.command(description="retrieve calendar")
     async def get(self, ctx):
 
-        URL = "https://acm.mines.edu/schedule/ical.ics"
-        response = requests.get(URL)
+        #get data
+        response = requests.get("https://acm.mines.edu/schedule/ical.ics")
         if not response:
             await ctx.respond("error: " + str(response))
-        data = response.text
-        print(data)
+        data = ((response.text).replace("\n ", "")).split("\r\n")
+        
+        #parse and write to database
+        async with aiosqlite.connect("data/database.db") as db:
+            #delete table if it already exists
+            await db.execute("DROP TABLE IF EXISTS events")
+            #create a new empty table
+            await db.execute("CREATE TABLE events(summary text, start text, end text, stamp text, uid integer PRIMARY KEY, description text, location text)")
+            #parse data
+            for l in data:
+                line = l.split(':', 1)
+                id = line[0].split(';', 1)[0]
+                if id == "SUMMARY":
+                    summary = line[1]
+                elif id == "DTSTART":
+                    start = line[1]
+                elif id == "DTEND":
+                    end = line[1]
+                elif id == "DTSTAMP":
+                    stamp = line[1]
+                elif id == "UID":
+                    uid = int(line[1])
+                elif id == "DESCRIPTION":
+                    description = line[1].replace("'", "")
+                elif id == "LOCATION":
+                    location = line[1]
+                elif line == ["END", "VEVENT"]:
+                    await db.execute("INSERT INTO events VALUES(\'"+summary+"\',\'"+start+"\',\'"+end+"\',\'"+stamp+"\',"+str(uid)+",\'"+description+"\',\'"+location+"\')")
+            #commit changes
+            await db.commit()
 
+        print("success")
         await ctx.respond("success")
 
 
