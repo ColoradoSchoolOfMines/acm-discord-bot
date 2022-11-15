0490ed153c8659e37fe584ac4fa620a37f7618cd
Jayden Pahukula <72293021+JaydenPahukula@users.noreply.github.com>
2022-11-15 13:59:32 -0700

    Improved opening database.db

--- cogs/calendar.py
+++ cogs/calendar.py
@@ -10,10 +10,10 @@ class Calendar(commands.Cog):
 
     def __init__(self, bot : discord.Bot):
         self.bot = bot
-        self.loop.start()
+        self.loopp.start()
 
     def cog_unload(self):
-        self.loop.stop()
+        self.loopp.stop()
 
     @calendar.command(description="retrieve calendar")
     async def get(self, ctx):
@@ -25,36 +25,34 @@ class Calendar(commands.Cog):
             await ctx.respond("error: " + str(response))
         data = ((response.text).replace("\n ", "")).split("\r\n")
         
-        #create data/ if it doesnt exist
-        if not os.path.exists("data/"):
-            os.mkdir("data/")
-        
         #open database
-        async with aiosqlite.connect("data/database.db") as db:
-            #drop table if it already exists
-            await db.execute(f"DROP TABLE IF EXISTS events_{GUILDID}")
-            #create a new empty table
-            await db.execute(f"CREATE TABLE events_{GUILDID}(summary text, start text, end text, uid integer PRIMARY KEY, description text, location text)")
-            #parse data
-            for l in data:
-                line = l.split(':', 1)
-                id = line[0].split(';', 1)[0]
-                if id == "SUMMARY":
-                    summary = line[1]
-                elif id == "DTSTART":
-                    start = line[1]
-                elif id == "DTEND":
-                    end = line[1]
-                elif id == "UID":
-                    uid = int(line[1])
-                elif id == "DESCRIPTION":
-                    description = line[1].replace("'", "")
-                elif id == "LOCATION":
-                    location = line[1]
-                elif line == ["END", "VEVENT"]:
-                    await db.execute(f"INSERT INTO events_{GUILDID} VALUES(\'{summary}\',\'{start}\',\'{end}\',{uid},\'{description}\',\'{location}\')")
-            #commit changes to database
-            await db.commit()
+        db = await self.openDatabase()
+        #drop table if it already exists
+        await db.execute(f"DROP TABLE IF EXISTS events_{GUILDID}")
+        #create a new empty table
+        await db.execute(f"CREATE TABLE events_{GUILDID}(summary text, start text, end text, uid integer PRIMARY KEY, description text, location text)")
+        #parse data
+        for l in data:
+            line = l.split(':', 1)
+            id = line[0].split(';', 1)[0]
+            if id == "SUMMARY":
+                summary = line[1]
+            elif id == "DTSTART":
+                start = line[1]
+            elif id == "DTEND":
+                end = line[1]
+            elif id == "UID":
+                uid = int(line[1])
+            elif id == "DESCRIPTION":
+                description = line[1].replace("'", "")
+            elif id == "LOCATION":
+                location = line[1]
+            elif line == ["END", "VEVENT"]:
+                await db.execute(f"INSERT INTO events_{GUILDID} VALUES(\'{summary}\',\'{start}\',\'{end}\',{uid},\'{description}\',\'{location}\')")
+        #commit changes to database
+        await db.commit()
+        #close database
+        await db.close()
 
         #test announcement
         await self.announce(await self.getchannel(ctx))
@@ -62,9 +60,8 @@ class Calendar(commands.Cog):
         print("successfully retrieved calendar")
         await ctx.respond("successfully retrieved calendar", ephemeral=True)
     
-
     @tasks.loop(seconds=5.0)
-    async def loop(self):
+    async def loopp(self):
         print("hello")
 
     async def announce(self, channel):
@@ -77,21 +74,19 @@ class Calendar(commands.Cog):
 
         GUILDID = str(ctx.guild_id)
 
-        #create data/ if it doesnt exist
-        if not os.path.exists("data/"):
-            os.mkdir("data/")
-
         #open database
-        async with aiosqlite.connect("data/database.db") as db:
-            #create announcement_channels table if it doesnt exist
-            await db.execute("CREATE TABLE IF NOT EXISTS announcement_channels(guildID integer, announcementChannel integer)")
-            #find channel id from table
-            async with db.execute(f"SELECT * FROM announcement_channels WHERE guildID={GUILDID}") as cursor:
-                list = await cursor.fetchall()
-            if list == []:
-                #if announcement channel not known, set the channel
-                channel = await self.setchannel(ctx)
-                list.append((GUILDID, channel.id))
+        db = await self.openDatabase()
+        #create announcement_channels table if it doesnt exist
+        await db.execute("CREATE TABLE IF NOT EXISTS announcement_channels(guildID integer, announcementChannel integer)")
+        #find channel id from table
+        async with db.execute(f"SELECT * FROM announcement_channels WHERE guildID={GUILDID}") as cursor:
+            list = await cursor.fetchall()
+        if list == []:
+            #if announcement channel not known, set the channel
+            channel = await self.setchannel(ctx)
+            list.append((GUILDID, channel.id))
+        #close database
+        await db.close()
 
         #find channel from id
         channel = discord.utils.get(ctx.guild.text_channels, id=list[0][1])
@@ -120,45 +115,50 @@ class Calendar(commands.Cog):
                 await interaction.response.send_message(f"Annoucement channel set to: `{channel.name}`", ephemeral=True)
                 return
 
-        #create data/ if it doesnt exist
-        if not os.path.exists("data/"):
-            os.mkdir("data/")
-
-        #find current channel
-        async with aiosqlite.connect("data/database.db") as db:
-            #create announcement_channels table if it doesnt exist
-            await db.execute("CREATE TABLE IF NOT EXISTS announcement_channels(guildID integer, announcementChannel integer)")
-            #find channel from table
-            async with db.execute(f"SELECT * FROM announcement_channels WHERE guildID={GUILDID}") as cursor:
-                list = await cursor.fetchall()
-                if list == []:
-                    #if announcement channel not known
-                    label = "there is no current channel set"
+        #open datbase
+        db = await self.openDatabase()
+        #create announcement_channels table if it doesnt exist
+        await db.execute("CREATE TABLE IF NOT EXISTS announcement_channels(guildID integer, announcementChannel integer)")
+        #find channel from table
+        async with db.execute(f"SELECT * FROM announcement_channels WHERE guildID={GUILDID}") as cursor:
+            list = await cursor.fetchall()
+            if list == []:
+                #if announcement channel not known
+                label = "there is no current channel set"
+            else:
+                #find current channel
+                currChannel = discord.utils.get(ctx.guild.text_channels, id=list[0][1])
+                if currChannel == None:
+                    label = "current channel is invalid"
                 else:
-                    #find current channel
-                    currChannel = discord.utils.get(ctx.guild.text_channels, id=list[0][1])
-                    if currChannel == None:
-                        label = "current channel is invalid"
-                    else:
-                        label = f"current channel is: {currChannel.name}"
+                    label = f"current channel is: {currChannel.name}"
 
-                #prompt user with modal
-                modal = Modal(label, title="Set Announcement Channel")
-                await ctx.send_modal(modal)
-                await modal.wait()
-                channel = discord.utils.get(ctx.guild.text_channels, id=int(modal.children[0].value))
+            #prompt user with modal
+            modal = Modal(label, title="Set Announcement Channel")
+            await ctx.send_modal(modal)
+            await modal.wait()
+            channel = discord.utils.get(ctx.guild.text_channels, id=int(modal.children[0].value))
 
-                if list == []:
-                    #if not known, insert new data
-                    await cursor.execute(f"INSERT INTO announcement_channels VALUES({GUILDID}, {channel.id})")
-                else:
-                    #if already known, update data
-                    await cursor.execute(f"UPDATE announcement_channels SET announcementChannel = {channel.id}")
-                await db.commit()
+            if list == []:
+                #if not known, insert new data
+                await cursor.execute(f"INSERT INTO announcement_channels VALUES({GUILDID}, {channel.id})")
+            else:
+                #if already known, update data
+                await cursor.execute(f"UPDATE announcement_channels SET announcementChannel = {channel.id}")
+        #commit changes to database
+        await db.commit()
+        #close database
+        await db.close()
 
         print(f"successfully set channel to: {channel.name} ({channel.id})")
         return channel
 
+    #opens database from: data/database.db
+    async def openDatabase(self):
+        #create data/ if it doesnt exist
+        if not os.path.exists("data/"):
+            os.mkdir("data/")
+        return await aiosqlite.connect("data/database.db")
     
 def setup(bot):
     bot.add_cog(Calendar(bot))
