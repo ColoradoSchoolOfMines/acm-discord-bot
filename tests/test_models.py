900e8ef00c2779c6e7e8e9a0970aa94b79963f2c
Keenan D. Buckley <keenandbuckley@protonmail.com>
2022-11-15 17:15:16 -0700

    Testing for cogs.roles.RoleTable and Testing refactor

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,7 +3,6 @@
 import os
 import sys
 import inspect
-import shutil
 
 current_dir = os.path.dirname(os.path.abspath(
     inspect.getfile(inspect.currentframe())))
@@ -11,28 +10,21 @@ parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
 
 import models
+import conftest
 
 def test_load_dot_env(dir: str = models.STATIC_DIR):
     """ Assert that loaded .env contains all keys in models.ENVs """
     models.load_dot_env(dir=dir)
     assert os.path.isfile(f'{dir}/{models.ENV_FILENAME}'), f'{models.ENV_FILENAME} doesn\'t exist'
     for env in models.ENVs:
-        assert (models.get_env_safe(env)
-                is not None), f'Can\'t find env \"{env.value}\"'
+        assert (models.get_env_safe(env) is not None), f'Can\'t find env \"{env.value}\"'
 
 def test_create_dot_env():
     """ Assert that a newly created .env contains all keys in models.ENVs """
-    TEST_DIR = "temp_testing"
-    assert not os.path.isfile(
-        f'{TEST_DIR}/{models.ENV_FILENAME}'), f'{TEST_DIR}/{models.ENV_FILENAME} already exists, unsafe to test with'
-    test_load_dot_env(dir=TEST_DIR)
-    assert os.path.isfile(
-        f'{TEST_DIR}/{models.ENV_FILENAME}'), f'{TEST_DIR}/{models.ENV_FILENAME} was not created'
-    shutil.rmtree(TEST_DIR)
+    assert not os.path.isfile(f'{conftest.TEST_DIR}/{models.ENV_FILENAME}'), f'{conftest.TEST_DIR}/{models.ENV_FILENAME} already exists, unsafe to test with'
+    test_load_dot_env(dir=conftest.TEST_DIR)
+    assert os.path.isfile(f'{conftest.TEST_DIR}/{models.ENV_FILENAME}'), f'{conftest.TEST_DIR}/{models.ENV_FILENAME} was not created'
 
 def test_create_dot_env_dir():
     """ Assert that models.create_dot_env() still works if directory already exists """
-    TEST_DIR = "temp_testing"
-    os.mkdir(TEST_DIR)
-    models.create_dot_env(dir=TEST_DIR)
-    shutil.rmtree(TEST_DIR)
+    models.create_dot_env(dir=conftest.TEST_DIR)
